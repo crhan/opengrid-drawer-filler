099ed04f09e1c8c7062fe5836979185a7bca2c0c
ruohan.chen <crhan123@gmail.com>
2026-03-01 22:43:00 +0800

    refactor: update core/__init__.py exports

--- opengrid/core/__init__.py
+++ opengrid/core/__init__.py
@@ -1,11 +1,15 @@
 """Core module exports"""
 from .constants import (
-    TILE_SIZE, MIN_TILE,
-    TILE_THICKNESS, PRESETS, FILAMENT_MAIN_PER_CELL, FILAMENT_SUPPORT_PER_CELL,
-    PRINT_TIME_PER_CELL, SWAP_PENALTY
+    TILE_SIZE,
+    MIN_TILE,
+    FILAMENT_MAIN_PER_CELL,
+    FILAMENT_SUPPORT_PER_CELL,
+    PRINT_TIME_PER_CELL,
+    SWAP_PENALTY,
+    PRESETS,
 )
-from .grid import get_max_stacks, get_grid_dimensions, validate_tile
+
+from .grid import GridConfig, get_grid_dimensions, validate_tile, get_max_stacks
+from .scheme import find_all_schemes, find_best_scheme, normalize_tiles
 from .splitter import split_with_limit, calc_balance, calc_scheme_balance
-from .scheme import find_best_scheme, find_all_schemes, normalize_tiles, validate_tiles
-from .cost import calculate_print_cost
-from .stats import calculate_filament_and_time, format_time
+from .split_result import PrinterConfig, SplitResult
